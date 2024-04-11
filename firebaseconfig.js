f8612f7630e13f9ae1dfcfddb4b3f7dc60778be8
Ville Kautiainen <kautiainen.ville@gmail.com>
2024-04-11 19:32:38 +0300

    Removed firebaseconfig.js from gitignore

---
+++ firebaseconfig.js
@@ -0,0 +1,15 @@
+
+// https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public
+const firebaseConfig = {
+  apiKey: "AIzaSyBl3au2sp5oK0wpPplBHIpl6ahkA7XZb-k",
+  authDomain: "forumproject-e0285.firebaseapp.com",
+  projectId: "forumproject-e0285",
+  storageBucket: "forumproject-e0285.appspot.com",
+  messagingSenderId: "596616422732",
+  appId: "1:596616422732:web:82d6cbb60c2a315c3fdbe9"
+};
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase
